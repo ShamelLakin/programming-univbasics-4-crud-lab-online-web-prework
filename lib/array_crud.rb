ce1124d296807f0d8a413314b7851fdcc71257fa
Shamel Lakin <christlife4u@gmail.com>
2019-08-13 17:26:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,8 @@ def add_element_to_end_of_array(create_an_array, scarf)
   
 end
 
-def add_element_to_start_of_array(create_an_array, element)
-  
+def add_element_to_start_of_array(create_an_array, muffs)
+  add_element_to_start_of_array.unshift()
 end
 
 def remove_element_from_end_of_array(array)
