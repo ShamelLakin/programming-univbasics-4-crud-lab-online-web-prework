5e9cb13f0026888bad5353771110c29cee3c54f6
Shamel Lakin <christlife4u@gmail.com>
2019-08-13 17:25:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(create_an_array, scarf)
   
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(create_an_array, element)
   
 end
 
