5c6a1f1241f6e5a02e1a6d48ee07df517e519347
Shamel Lakin <christlife4u@gmail.com>
2019-08-13 17:38:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,33 +8,40 @@ end
 
 def add_element_to_end_of_array(create_an_array, scarf)
   add_element_to_end_of_array << "scarf"
-  
+  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(create_an_array, muffs)
   add_element_to_start_of_array.unshift("muffs")
+  p add_element_to_start_of_array
 end
 
 def remove_element_from_end_of_array(create_an_array)
   scarf = remove_element_from_end_of_array.pop
+  p remove_element_from_end_of_array
 end
 
 def remove_element_from_start_of_array(create_an_array)
   muffs = remove_element_from_start_of_array.shift
+  p remove_element_from_start_of_array
 end
 
 def retrieve_element_from_index(create_an_array, [2])
+  
+  p retrieve_element_from_index
   
 end
 
 def retrieve_first_element_from_array(create_an_array)
   
+  p retrieve_first_element_from_array
 end
 
 def retrieve_last_element_from_array(array)
   
+  p retrieve_last_element_from_array
 end
 
 def update_element_from_index(array, index_number, element)
-
+  p update_element_from_index
 end
